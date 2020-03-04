bd8bcdfd5d5e06d087cbb42ccf19198e9ec1476a
healthabits <rafailia.asimakopoulou@hva.nl>
2020-03-04 20:29:46 +0100

    commiit

--- assets/bots/home/home-bot.js
+++ assets/bots/home/home-bot.js
@@ -402,7 +402,7 @@ var results = function () {
       document.getElementById('profile').appendChild(x);
    }
    // LIFE AND TECH CONSCIOUS
-    if ((selections[7] == "healthy" || selections[7]=="less healthy") &&  analyze(selections[0])>0 && analyze(selections[9])>0) {
+    if (analyze(selections[3])<=0 && (selections[7] == "healthy" || selections[7]=="less healthy") &&  analyze(selections[0])>0 && analyze(selections[9])>0) {
       
       document.getElementById("home-demo").style.display="none"; 
       document.getElementById('profile').style.display="block";
@@ -445,7 +445,7 @@ var results = function () {
     
     }
     // TECH SKEPTIC
-    if ( analyze(selections[0])<=0) {
+    if (analyze(selections[0])<=0 && selections[8] != "mostly unhealthy" && selections[8] != "unhealthy" && analyze(selections[9]) >0) {
       
       document.getElementById("home-demo").style.display="none"; 
       document.getElementById('profile').style.display="block";
@@ -488,7 +488,7 @@ var results = function () {
     
     }
         // IGNORANT
-        if ((selections[7] != "healthy") && analyze(selections[0]) <= 0 && (analyze(selections[9])<=0 )) {
+        if ((selections[7] != "healthy") && selections[8]  == "maybe in the future" || selections[8] == "not at all" && analyze(selections[0]) <= 0 && (analyze(selections[9])<=0 )) {
       
           document.getElementById("home-demo").style.display="none"; 
       document.getElementById('profile').style.display="block";
@@ -531,7 +531,7 @@ var results = function () {
         
         }
           // BLOCKED
-          if ((selections[8] !="heavy activities") && (analyze(selections[9])<=0 || analyze(selections[9])>0) ) {
+          if (selections[7] != "healthy" && selections[7] != "less healthy" && (selections[8] !="heavy activities" && selections[8] != "light activities") && (analyze(selections[9])>0) ) {
       
             document.getElementById("home-demo").style.display="none"; 
       document.getElementById('profile').style.display="block";
